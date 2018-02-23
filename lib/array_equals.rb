f9fbecf0f6dd4c07b57b5b1cfcbc208785e36a46
Kat <kat.eastman@gmail.com>
2018-02-22 23:52:40 -0800

    green tests - compared arrays without using array class methods to compare the whole array at once.

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,28 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
+# cannot use ruby array methods like array1 == array2, or array1 & array2, or array1 - array2
 def array_equals(array1, array2)
-  raise NotImplementedError
+
+  if array1 == nil && array2 == nil
+      return true
+    elsif array1 == nil || array2 == nil
+      return false
+  end
+
+  if array1.length != array2.length
+    return false
+  elsif array1.length == array2.length
+    if array1.length == 0
+      return true
+    else
+      match = nil
+      array1.length.times do |i|
+        array1[i] != array2[i] ? match = false : match = true
+        if match == false
+          return match
+        end
+      end
+      return match
+    end
+  end
 end
