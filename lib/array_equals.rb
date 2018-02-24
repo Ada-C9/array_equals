9a72953962878de047c9bf0d0b53b1daed3d421f
Phoebe <phoebe.manning8@gmail.com>
2018-02-24 14:34:04 -0800

    array_equals method

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,26 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  # checks nil cases first bc you cannot index nil
+  if array1 == nil || array2 == nil
+    if array1 == nil && array2 == nil
+      return true # arrays both equal to nil
+    end
+    return false # one array equals nil
+  else
+    # if lengths of arrays are different, they are not equal
+    if array1.length == array2.length
+      i = 0
+      until array1[i] == nil # could also used .length method here
+        if array1[i] != array2[i]
+          return false
+        end
+        i += 1
+      end
+      return true # arrays are equal
+    else
+      return false #arrays are not the same length
+    end
+  end
+  # raise NotImplementedError
 end
--- specs/array_equals_spec.rb
+++ specs/array_equals_spec.rb
@@ -2,6 +2,8 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/array_equals'
 
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 describe "array equals" do
   describe "basic tests" do
     it "arrays are equal" do
