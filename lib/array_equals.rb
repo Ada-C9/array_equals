4af503ea1bdd4d0e7da020b73723df81aba7d411
Samantha Berk <sberk10@gmail.com>
2018-02-25 10:31:49 -0800

    added nested conditionals to check if 2 arrays have the same length and/or same elements in the same order

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,19 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  if array1 == nil && array2 == nil
+    return true
+  elsif array1 == nil && array2 != nil
+    return false
+  elsif array2 == nil && array1 != nil
+    return false
+  elsif array1.length == array2.length
+    if array1 == array2
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
 end
