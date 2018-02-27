3ab0941034872bb7fa15ee448137d416d2a1e5e0
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-02-26 16:22:17 -0800

    I updated the method to successfully pass all the tests.

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -8,19 +8,19 @@ require 'pry'
 def array_equals(array1, array2)
   # raise NotImplementedError
   # binding.pry
-  if array1 == nil && array2 != nil || array1 != nil && array2 == nil || array1.length != array2.length
-    return false
-  elsif array1.include?(nil) && array2.include?(nil)
+  if array1 == nil && array2 == nil
     return true
+  elsif array1 == nil && array2 != nil || array1 != nil && array2 == nil || array1.length != array2.length
+    return false
   elsif array1.length == array2.length
     array1.length.times do |i|
       if array1[i] == array2[i]
-        return true
         i += 1
       else
         return false
       end
     end
+    return true
   end
 
 end
