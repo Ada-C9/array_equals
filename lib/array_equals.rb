4f6c374c4b724a8af960d23570483fb854483869
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-27 21:44:04 -0800

    added method that will check if elements within an array and array size are equivalent.

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,33 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
+# def array_equals(array1, array2)
+#   raise NotImplementedError
+# end
+
 def array_equals(array1, array2)
-  raise NotImplementedError
+  index = 0
+  arrays_are_equal = true
+  checking_arrays = true
+  #checks for nil inputs
+  if array1.class == NilClass && array2.class == NilClass
+    arrays_are_equal = true
+    #checks if equivlanet class types or if arrays are the same length
+  elsif array1.class != array2.class || array1.length != array2.length
+    arrays_are_equal = false
+    #now we know we have arrays of the same length. check content
+  else
+    while checking_arrays
+      if array1[index] == array2[index]
+        if index == array1.length
+          checking_arrays = !checking_arrays
+        end
+        index+=1
+      else
+        arrays_are_equal = !arrays_are_equal
+        checking_arrays = !checking_arrays
+      end
+    end
+
+  end
+  return arrays_are_equal
 end
