5a404555de9289682b0a3ca3075de648521826f9
Angela Poland <angelalpoland@gmail.com>
2018-02-27 22:42:25 -0800

    refactored down since earlier version was not passing all tests. seems like I was overthinking it.

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -4,22 +4,25 @@ def array_equals(array1, array2)
   #raise NotImplementedError
 
 
-  if array1 == nil && array2 == nil
-    return true
-  elsif (array1 == nil && array2 != nil) || (array1 != nil && array2 == nil)
-    return  false
-  elsif array1.length != array2.length
-        return false
-  elsif array1.length == array2.length
-    array1.length.times do |num|
-      if array1[num] != array2[num]
-        return false
-      else
-        return true
-      end
-    end
-  end
 
+    return array1 == array2 ? true : false
+
+  # code below does not pass test if array is empty
+  # if array1 == nil && array2 == nil
+  #   return true
+  # elsif (array1 == nil && array2 != nil) || (array1 != nil && array2 == nil)
+  #   return  false
+  # elsif array1.length != array2.length
+  #       return false
+  # elsif array1.length == array2.length
+  #   array1.length.times do |num|
+  #     if array1[num] != array2[num]
+  #       return false
+  #     else
+  #       return true
+  #     end
+  #   end
+  # end
 
 
 end
