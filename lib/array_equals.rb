7100e71010d6588eee4984b399b04efb4e7470a2
Angelica Ceja Morales <35648514+amcejamorales@users.noreply.github.com>
2018-03-24 19:53:10 -0700

    Rework method logic

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -2,23 +2,21 @@ require "pry"
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  largest_array, smallest_array = [], []
-  array1 = [nil] if array1.nil?
-  array2 = [nil] if array2.nil?
-
-  if array1.length > array2.length
-    largest_array = array1
-    smallest_array = array2
-  else
-    largest_array = array2
-    smallest_array = array1
+  if array1.nil? && array2.nil?
+    return true
+  elsif array1.nil? || array2.nil?
+    return false
   end
 
-  equal = true
+  return false if array1.length != array2.length
 
-  largest_array.each_with_index do |value, index|
-    equal = value == smallest_array[index]
+  array_index = 0
+
+  while array_index < array1.length
+    return false if array1[array_index] != array2[array_index]
+    array_index += 1
   end
 
-  return equal
+  return true
+
 end
